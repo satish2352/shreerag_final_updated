72ea915d423d2ffd91aa17077dcae34f353a2c08
Satish <satishaurange@sumagoinfotech.com>
2024-06-11 14:18:35 +0530

    changes for the title in purchase order

--- app/Http/Controllers/Organizations/Purchase/PurchaseOrderController.php
+++ app/Http/Controllers/Organizations/Purchase/PurchaseOrderController.php
@@ -25,7 +25,6 @@ class PurchaseOrderController extends Controller
         return view(
             'organizations.purchase.addpurchasedetails.list-purchase-orders',
             compact(
-                'title',
                 'getOutput',
                 'requistition_id'
             )
