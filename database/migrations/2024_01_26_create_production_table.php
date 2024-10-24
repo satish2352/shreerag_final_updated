4f6d8671ffb760417ead1bb3cb3f43535b94aec4
Savita444 <savita.hajare@sumagoinfotech.com>
2024-10-24 11:20:03 +0530

    changes

--- database/migrations/2024_01_26_create_production_table.php
+++ database/migrations/2024_01_26_create_production_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->unsignedBigInteger('design_id');
             $table->unsignedBigInteger('is_approved_production')->nullable();
             $table->string('production_status_quantity_tracking')->nullable();
+            $table->string('store_status_quantity_tracking')->nullable();
             $table->boolean('is_approve')->default(false);
             $table->boolean('is_active')->default(true);
             $table->boolean('is_deleted')->default(false);
