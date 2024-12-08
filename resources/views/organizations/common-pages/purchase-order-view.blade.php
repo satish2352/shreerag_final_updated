0f9d495bb545b00d19f1b999033a0633e9b442b9
Savita444 <savita.hajare@sumagoinfotech.com>
2024-12-08 16:03:56 +0530

    changes

--- resources/views/organizations/common-pages/purchase-order-view.blade.php
+++ resources/views/organizations/common-pages/purchase-order-view.blade.php
@@ -228,7 +228,7 @@
                                             <strong>Prepared By</strong>
                                         </td>
                                         <td class="no-border" colspan="2" style="padding-bottom: 10px; text-align:center;">( Finance Signatory )</td>
-                                        <td class="no-border" colspan="2" style="padding-bottom: 10px;">( Purchase Signatory )</td>
+                                        <td class="no-border" colspan="3" style="padding-bottom: 10px;">( Purchase Signatory )</td>
                                         <td class="no-border" colspan="2" style="display:inline-block; text-align:center; padding-bottom: 10px;">(Authorized Signatory)</td>
                                     </tr>
                                 </tfoot>
