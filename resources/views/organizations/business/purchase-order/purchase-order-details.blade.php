2681061f331160c9ebc0bec7303e153480ababc1
Savita444 <savita.hajare@sumagoinfotech.com>
2024-12-11 12:51:59 +0530

    changes

--- resources/views/organizations/business/purchase-order/purchase-order-details.blade.php
+++ resources/views/organizations/business/purchase-order/purchase-order-details.blade.php
@@ -6,7 +6,7 @@
 
    
     @else
-    <div class="" style="margin-bottom: 70px;">
+    <div class="" style="padding-bottom: 100px; padding-left: 20px; ">
         <a href="{{ route('accept-purchase-order', ['purchase_order_id' => $purchase_order_id, 'business_id' => $purchaseOrder->business_details_id]) }}"><button data-toggle="tooltip"
                 title="Accept Purchase Order" class="pd-setting-ed">Accept</button></a> &nbsp;
         &nbsp; &nbsp;
