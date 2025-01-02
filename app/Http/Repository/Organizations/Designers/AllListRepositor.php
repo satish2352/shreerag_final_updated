82a7adc7c7c261ed306c5955a53cee0685ed39c5
Savita444 <savita.hajare@sumagoinfotech.com>
2025-01-02 16:56:41 +0530

    changes in code

--- app/Http/Repository/Organizations/Designers/AllListRepositor.php
+++ app/Http/Repository/Organizations/Designers/AllListRepositor.php
@@ -204,59 +204,47 @@ public function getAllListDesignRecievedForCorrection() {
   try {
       $array_to_be_check = [config('constants.PRODUCTION_DEPARTMENT.DESIGN_SENT_TO_DESIGN_DEPT_FOR_REVISED')];
 
-      $data_output = BusinessApplicationProcesses::leftJoin('production', function($join) {
+      $data_output = BusinessApplicationProcesses::leftJoin('production', function ($join) {
               $join->on('business_application_processes.business_details_id', '=', 'production.business_details_id');
           })
-          ->leftJoin('designs', function($join) {
+          ->leftJoin('designs', function ($join) {
               $join->on('business_application_processes.business_details_id', '=', 'designs.business_details_id');
           })
-          ->leftJoin('businesses_details', function($join) {
+          ->leftJoin('businesses_details', function ($join) {
               $join->on('production.business_details_id', '=', 'businesses_details.id');
           })
-          ->leftJoin('businesses', function($join) {
+          ->leftJoin('businesses', function ($join) {
               $join->on('business_application_processes.business_id', '=', 'businesses.id');
           })
-          ->leftJoin('design_revision_for_prod', function($join) {
+          ->leftJoin('design_revision_for_prod', function ($join) {
               $join->on('business_application_processes.business_details_id', '=', 'design_revision_for_prod.business_details_id');
           })
           ->whereIn('business_application_processes.production_status_id', $array_to_be_check)
           ->where('businesses.is_active', true)
+          ->select(
+              'businesses.id as business_id',
+              'businesses_details.id as detail_id',
+              'businesses.customer_po_number',
+              'businesses_details.product_name',
+              'businesses_details.quantity',
+              'businesses_details.description',
+              'businesses.remarks',
+              'designs.bom_image',
+              'designs.design_image',
+              DB::raw('COALESCE(design_revision_for_prod.reject_reason_prod, "") as reject_reason_prod')
+          )
           ->groupBy(
               'businesses.id',
               'businesses_details.id',
-              'businesses.title',
               'businesses.customer_po_number',
               'businesses_details.product_name',
               'businesses_details.quantity',
               'businesses_details.description',
               'businesses.remarks',
-              'businesses.is_active',
-              'production.business_id',
               'designs.bom_image',
               'designs.design_image',
-              'design_revision_for_prod.id',
-              'design_revision_for_prod.bom_image',
-              'design_revision_for_prod.design_image'
+              'design_revision_for_prod.reject_reason_prod'
           )
-          ->select(
-              'businesses.id',
-              'businesses_details.id',
-              'businesses.title',
-              'businesses.customer_po_number',
-              'businesses_details.product_name',
-              'businesses_details.quantity',
-              'businesses_details.description',
-              'businesses.remarks',
-              'businesses.is_active',
-              'production.business_id',
-              'designs.bom_image',
-              'designs.design_image',
-              DB::raw('COALESCE(design_revision_for_prod.reject_reason_prod, "") as reject_reason_prod'),
-              'design_revision_for_prod.id as design_revision_for_prod_id',
-              'design_revision_for_prod.bom_image as re_bom_image',
-              'design_revision_for_prod.design_image as re_design_image'
-          )
-          ->distinct('businesses_details.id')
           ->orderBy('businesses.updated_at', 'desc')
           ->get();
 
