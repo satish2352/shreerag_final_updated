73941cd95f96e15dde68ca573edf1abfbc66e158
Savita444 <savita.hajare@sumagoinfotech.com>
2025-01-02 17:17:54 +0530

    changes in code

--- app/Http/Repository/Organizations/Designers/AllListRepositor.php
+++ app/Http/Repository/Organizations/Designers/AllListRepositor.php
@@ -231,7 +231,7 @@ public function getAllListDesignRecievedForCorrection() {
               'businesses.remarks',
               'designs.bom_image',
               'designs.design_image',
-              DB::raw('COALESCE(design_revision_for_prod.reject_reason_prod, "") as reject_reason_prod')
+              DB::raw('MAX(COALESCE(design_revision_for_prod.reject_reason_prod, "")) as reject_reason_prod'),
           )
           ->groupBy(
               'businesses.id',
