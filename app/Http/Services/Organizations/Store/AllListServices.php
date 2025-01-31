85e91ce1544640248e426dd48252170cb7cb809d
Savita444 <savita.hajare@sumagoinfotech.com>
2025-01-31 17:01:43 +0530

    change in finance add grn

--- app/Http/Services/Organizations/Store/AllListServices.php
+++ app/Http/Services/Organizations/Store/AllListServices.php
@@ -88,8 +88,6 @@ class AllListServices
     {
         try {
             $data_output = $this->repo->getAllListMaterialReceivedFromQualityPOTrackingBusinessWise($purchase_order_id);
-            dd($data_output);
-            die();
             return $data_output;
         } catch (\Exception $e) {
             return $e;
