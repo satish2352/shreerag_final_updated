895c301da8c73138c17e6f9cd0c31c7469f162fa
Savita444 <savita.hajare@sumagoinfotech.com>
2025-02-25 15:54:34 +0530

    change in soft delete

---
+++ resources/views/admin/pages/dashboard/dashboard1.blade.php
@@ -0,0 +1,197 @@
+<!-- Static Table Start -->
+@extends('admin.layouts.master')
+@section('content')
+<!-- ============= pratiksha (21/08/24) =============  -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+    integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous">
+</script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+
+<style>
+@import url("https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.4.0/font/bootstrap-icons.min.css");
+
+.btn-primary {
+    background-color: #25385F;
+    border-color: #25385F;
+}
+
+.btn-primary:hover {
+    background-color: #25385F;
+    border-color: #25385F;
+}
+
+.card {
+    border: 0;
+    border-radius: 0.375rem;
+}
+
+.card.shadow {
+    box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.075);
+}
+
+.d-flex {
+    display: flex;
+}
+
+.flex-column {
+    flex-direction: column;
+}
+
+.flex-lg-row {
+    flex-direction: row;
+}
+
+.h-lg-full {
+    height: 100%;
+}
+
+.h-screen {
+    height: 100vh;
+}
+
+.overflow-y-lg-auto {
+    overflow-y: auto;
+}
+
+.icon-shape {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 3rem;
+    height: 3rem;
+    border-radius: 50%;
+}
+
+.bg-tertiary {
+    background-color: #6c757d;
+}
+
+.bg-primary {
+    background-color: #25385F;
+}
+
+.bg-info {
+    background-color: #17a2b8;
+}
+
+.bg-warning {
+    background-color: #31497D !important;
+}
+
+.text-white {
+    color: #fff;
+}
+
+.badge-pill {
+    border-radius: 50rem;
+    float: right;
+}
+
+.bg-soft-success {
+    background-color: #d4edda;
+}
+
+.bg-soft-danger {
+    background-color: #f8d7da;
+}
+
+.text-success {
+    color: #28a745;
+}
+
+.text-danger {
+    color: #dc3545;
+}
+
+.h6 {
+    font-size: 1.25rem;
+}
+
+.font-semibold {
+    font-weight: 600;
+}
+
+.font-bold {
+    font-weight: 700;
+}
+
+.text-muted {
+    color: #6c757d;
+}
+
+.text-nowrap {
+    white-space: nowrap;
+    font-size: 15px !important;
+    font-weight: bold;
+}
+
+.text-nowrap a {
+    text-decoration: none !important;
+}
+
+.text-xs {
+    font-size: 0.75rem;
+}
+
+.py-6 {
+    padding-top: 1.5rem;
+    padding-bottom: 1.5rem;
+}
+
+.px-2 {
+    padding-left: 0.5rem;
+    padding-right: 0.5rem;
+}
+
+.pe-2 {
+    padding-right: 0.5rem;
+}
+
+.px-0 {
+    padding-left: 0;
+    padding-right: 0;
+}
+
+.mx-n1 {
+    margin-left: -0.25rem;
+    margin-right: -0.25rem;
+}
+
+.mb-npx {
+    margin-bottom: -0.5rem;
+}
+
+.text-sm a {
+    text-decoration: none;
+    font-size: larger;
+}
+
+/* ======================= */
+.right-side {
+    margin-bottom: 5px;
+    padding: 5px;
+    border-radius: 5px;
+    font-weight: bold;
+    text-transform: capitalize;
+}
+.accordion-button {
+    /* background-color: #dee2e6 !important; */
+    background-color: #ffff !important;
+    color: black;
+    text-transform: capitalize;
+}
+.accordion-button.active {
+    background-color: #243772 !important;
+    color: white;
+}
+</style>
+
+<div class="data-table-area mg-tb-15">
+    <h1>test</h1>
+</div>
+@endsection
