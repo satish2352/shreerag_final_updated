9ffde5f7dd8c265bfb0be8c1cbd7856fdc6ec80a
Savita444 <savita.hajare@sumagoinfotech.com>
2025-02-27 10:46:38 +0530

    change in production

--- app/Http/Controllers/Admin/Dashboard/DashboardController.php
+++ app/Http/Controllers/Admin/Dashboard/DashboardController.php
@@ -321,12 +321,24 @@ $business_received_for_designs= DesignModel::leftJoin('businesses', function($jo
         ->where('is_deleted', 0)
         ->where('is_active',1)->count();
 
-        $material_received_for_production = BusinessApplicationProcesses::leftJoin('businesses', function ($join) {
+        $material_received_for_production =BusinessApplicationProcesses::leftJoin('production', function ($join) {
+            $join->on('business_application_processes.business_details_id', '=', 'production.business_details_id');
+        })
+        ->leftJoin('designs', function ($join) {
+            $join->on('business_application_processes.business_details_id', '=', 'designs.business_details_id');
+        })
+        ->leftJoin('businesses', function ($join) {
             $join->on('business_application_processes.business_id', '=', 'businesses.id');
         })
-            ->leftJoin('production', function($join) {
-            $join->on('business_application_processes.business_id', '=', 'production.business_id');
-          })
+        ->leftJoin('businesses_details', function ($join) {
+            $join->on('business_application_processes.business_details_id', '=', 'businesses_details.id');
+        })
+        ->leftJoin('design_revision_for_prod', function ($join) {
+            $join->on('business_application_processes.business_details_id', '=', 'design_revision_for_prod.business_details_id');
+        })
+        ->leftJoin('purchase_orders', function ($join) {
+            $join->on('business_application_processes.business_details_id', '=', 'purchase_orders.business_details_id');
+        })
           ->where('business_application_processes.off_canvas_status', 17)
           ->where('production.production_status_quantity_tracking', 'incomplete')
         ->where('businesses.is_deleted', 0)
