2af5efefc243c73653ba2d22b0847a465db170eb
Savita Hajare <hajaresavita04@gmail.com>
2025-11-29 18:12:06 +0530

    solve testing issues and remove unwanted code

--- database/migrations/2025_08_19_175136_create_login_history.php
+++ database/migrations/2025_08_19_175136_create_login_history.php
@@ -16,10 +16,10 @@ return new class extends Migration
         Schema::create('login_history', function (Blueprint $table) {
           $table->bigIncrements('id');
              $table->unsignedBigInteger('user_id')->nullable();
-             $table->string('ip_address', 45)->nullable();
             $table->string('location_address')->nullable();
              $table->decimal('latitude', 10, 7)->nullable();
             $table->decimal('longitude', 10, 7)->nullable();
+              $table->string('ip_address')->nullable();
             $table->boolean('is_active')->default(true);
             $table->boolean('is_deleted')->default(false);
             $table->timestamps();
