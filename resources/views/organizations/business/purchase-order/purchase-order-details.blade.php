392c4f5729c8c49e251f6b2a997b02ce8cd9c2be
Savita Hajare <hajaresavita04@gmail.com>
2026-06-15 11:53:45 +0530

    add print button and updated date field in purchase depratment agains

--- resources/views/organizations/business/purchase-order/purchase-order-details.blade.php
+++ resources/views/organizations/business/purchase-order/purchase-order-details.blade.php
@@ -2,18 +2,19 @@
 @section('content')
     @include('organizations.common-pages.purchase-order-view')
 
-    @if ($purchaseOrder->purchase_status_from_owner == 1127)
-    @else
-        <div class="" style="padding-bottom: 100px; padding-left: 20px; ">
+    <div style="display: flex; align-items: center; gap: 12px; padding-left: 20px; padding-bottom: 100px;">
+        <button data-toggle="tooltip" title="Print Purchase Order" onclick="printInvoice()" type="button"
+            class="print-button"
+            style="padding: 8px 15px; font-size: 16px; border-radius: 3px; border: 1px solid rgba(0, 0, 0, .12); background: #007bff; color: #fff; cursor: pointer;">Print</button>
+
+        @if ($purchaseOrder->purchase_status_from_owner != 1127)
             <a
                 href="{{ route('accept-purchase-order', ['purchase_order_id' => $purchase_order_id, 'business_id' => $purchaseOrder->business_details_id]) }}"><button
-                    data-toggle="tooltip" title="Accept Purchase Order" class="accept-btn">Accept</button></a> &nbsp;
-            &nbsp; &nbsp;
+                    data-toggle="tooltip" title="Accept Purchase Order" class="accept-btn">Accept</button></a>
 
             <a
                 href="{{ route('rejected-purchase-order', ['purchase_order_id' => $purchase_order_id, 'business_id' => $purchaseOrder->business_details_id]) }}"><button
-                    data-toggle="tooltip" title="Rejected Purchase Order" class="reject-btn">Reject</button></a> &nbsp;
-            &nbsp; &nbsp;
-        </div>
-    @endif
+                    data-toggle="tooltip" title="Rejected Purchase Order" class="reject-btn">Reject</button></a>
+        @endif
+    </div>
 @endsection
--- resources/views/organizations/purchase/purchase/purchase-order-details.blade.php
+++ resources/views/organizations/purchase/purchase/purchase-order-details.blade.php
@@ -1,15 +1,17 @@
 @extends('admin.layouts.master')
 @section('content')
     @include('organizations.common-pages.purchase-order-view')
-  
-    @if($purchaseOrder->purchase_status_from_owner == '1127' &&  $purchaseOrder->purchase_status_from_purchase == '1126')
-    <div class="" style="padding-bottom:130px; padding-left: 17px; margin-top: 20px;">
-        <a href="{{ route('finalize-and-submit-mail-to-vendor',  ['purchase_order_id' => $purchase_order_id, 'business_id' => $purchaseOrder->business_details_id]) }}"><button data-toggle="tooltip"
-                title="Send Mail" class="accept-btn">Send Mail To
-                Vendor</button></a>
-    </div>
-     @else 
 
-    @endif 
+    <div style="display: flex; align-items: center; gap: 12px; padding-left: 17px; padding-bottom: 130px; margin-top: 20px;">
+        <button data-toggle="tooltip" title="Print Purchase Order" onclick="printInvoice()" type="button"
+            class="print-button"
+            style="padding: 8px 15px; font-size: 16px; border-radius: 3px; border: 1px solid rgba(0, 0, 0, .12); background: #007bff; color: #fff; cursor: pointer;">Print</button>
+
+        @if($purchaseOrder->purchase_status_from_owner == '1127' &&  $purchaseOrder->purchase_status_from_purchase == '1126')
+            <a href="{{ route('finalize-and-submit-mail-to-vendor',  ['purchase_order_id' => $purchase_order_id, 'business_id' => $purchaseOrder->business_details_id]) }}"><button data-toggle="tooltip"
+                    title="Send Mail" class="accept-btn">Send Mail To
+                    Vendor</button></a>
+        @endif
+    </div>
 @endsection
 
